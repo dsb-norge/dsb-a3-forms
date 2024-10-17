1c5b5026a3ba4391cfc7eef7943986bb39105d21
Jostein Gjertsen <josteingjertsen@gmail.com>
2024-10-17 15:44:56 +0200

    WIP: AuthClient for MT and FG

---
+++ src/DsbNorge.A3Forms/Clients/Auth/IAuthClient.cs
@@ -0,0 +1,6 @@
+﻿namespace DsbNorge.A3Forms.Clients.Auth;
+
+public abstract class IAuthClient
+{
+    public Task<string> GetToken(string clientIdConfig, string clientSecretConfig, string clientAuthUrlConfig);
+}

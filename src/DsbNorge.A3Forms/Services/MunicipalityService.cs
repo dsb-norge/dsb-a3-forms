231ddf744c3d4079bed51fd28733e755cb07f8fe
Simen Røkaas <simen.rokaas@dsb.no>
2025-01-31 15:03:32 +0100

    Simplified; provider -> service

--- src/DsbNorge.A3Forms/Services/MunicipalityService.cs
+++ src/DsbNorge.A3Forms/Services/MunicipalityService.cs
@@ -1,21 +1,18 @@
 ﻿using System.Globalization;
-using Altinn.App.Core.Features;
 using Altinn.App.Core.Models;
 using DsbNorge.A3Forms.Clients.GeoNorge;
 using DsbNorge.A3Forms.Models;
 
-namespace DsbNorge.A3Forms.Providers;
+namespace DsbNorge.A3Forms.Services;
 
-public class MunicipalityProvider(IGeoNorgeClient geoNorgeClient, string? id = null) : IAppOptionsProvider
+public class MunicipalityService(IGeoNorgeClient geoNorgeClient)
 {
-    public string Id { get; } = id ?? "municipalities";
-    
     public async Task<List<Municipality>> GetMunicipalities()
     {
         return await geoNorgeClient.GetMunicipalities();
     }
 
-    public async Task<AppOptions> GetAppOptionsAsync(string? language, Dictionary<string, string> keyValuePairs)
+    public async Task<AppOptions> GetAppOptionsAsync()
     {
         var municipalities = await GetMunicipalities();
 

4ca41143928fefdf994c0fe8ab6fd39073c1e820
Simen Røkaas <simen.rokaas@dsb.no>
2025-02-04 18:31:16 +0100

    Added extension for string IsNullOrBlank

--- src/DsbNorge.A3Forms/Common/Extensions.cs
+++ src/DsbNorge.A3Forms/Common/Extensions.cs
@@ -2,7 +2,12 @@ namespace DsbNorge.A3Forms.Common;
 
 public static class Extensions
 {
-    // fixing brain-dead C# which does not include this for lists as for strings
+    // fixing brain-dead C# which does not include this for lists and strings
+    public static bool IsNullOrEmpty(this string value)
+    {
+        return string.IsNullOrEmpty(value);
+    }
+    
     public static bool IsNullOrEmpty<T>(this List<T>? list)
     {
         return list == null || list.Count == 0;
